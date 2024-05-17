aa962f14d89355f0b3a5f4540716c9ad06a44af4
Kuro <juliojtp4598@gmail.com>
2024-05-17 10:55:55 -0500

    Flujo Actual Feature
    Se agrega la vista del flujo actual con los semáforos en el mapa

--- src/app/pages/maps/google/google.component.ts
+++ src/app/pages/maps/google/google.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit, Input, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, OnInit, Input, Inject, PLATFORM_ID, AfterViewInit, ViewChild, SimpleChanges, OnChanges } from '@angular/core';
+import { GoogleMap, MapInfoWindow, MapMarker } from '@angular/google-maps';
+
+
 
 interface MarkerProperties {
   position: {
     lat: number;
     lng: number;
   }
+  title?: string;
+  icon?: google.maps.Icon;
 };
 @Component({
   selector: 'app-google',
@@ -15,17 +20,25 @@ interface MarkerProperties {
 /**
  * Google component
  */
-export class GoogleComponent implements OnInit {
+export class GoogleComponent implements OnInit,AfterViewInit  {
+  @ViewChild(GoogleMap) mapComponent: GoogleMap;
+  @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
+  @Input() pitch: number = 10;
+  @Input() scrollwheel: boolean = false; //coordenadas de Lima
   longitude = -77.028333;
   latitude = -12.043333;
-  zoom: number = 9;
 
-  @Input() pitch: number = 10;
-  @Input() scrollwheel: boolean = false;
-  center: any;
-
+  tlIcon: string = 'assets/images/traffic-light.png';
+  redCycle: number = 262;   // Tiempo del ciclo rojo en segundos
+  greenCycle: number = 195
+  trafficMap : any;
+  currentZoom: number;
+  minZoomVisible: number = 18;
+  markersVisible: boolean = false;
+  circleCenter: google.maps.LatLngLiteral = { lat: -12.091756905999354, lng: -76.95300742653058 };
   // bread crumb items
   breadCrumbItems: Array<{}>;
+  infoContent = `<h1>Tiempo Semafórico Actual</h1><h2>🔴 Ciclo Rojo: ${this.redCycle}s</h2><h2>🟢 Ciclo Verde: ${this.greenCycle}s</h2>`;
   constructor(@Inject(PLATFORM_ID) private platformId: any,
     // private mapsAPILoader: MapsAPILoader
   ) { }
@@ -33,15 +46,51 @@ export class GoogleComponent implements OnInit {
 
   ngOnInit(): void {
     this.breadCrumbItems = [{ label: 'Maps' }, { label: 'Google Maps', active: true }];
+
   }
+
+  ngAfterViewInit() {
+    this.addTrafficLayer();
+    // Listener para el evento de cambio de zoom
+    this.mapComponent.googleMap.addListener('zoom_changed', () => {
+      this.currentZoom = this.mapComponent.googleMap.getZoom();
+      console.log('Nivel de zoom actual:', this.currentZoom);
+      });
+    }
+
+
   mapOptions: google.maps.MapOptions = {
-    center: { lat: 48.8588548, lng: 2.347035 },
-    zoom: 13,
+    mapId: "34e63ce8f10982d",
+    center: { lat: -12.04318, lng: -77.02824 },
+    zoom: 12,
+    zoomControl: false,
+    mapTypeControl: true,
+    minZoom: 12,
+    //maxZoom: 20,
   };
 
-  markers: MarkerProperties[] = [
-    { position: { lat: 48.8584, lng: 2.2945 } }, // Eiffel Tower
-    { position: { lat: 48.8606, lng: 2.3376 } }, // Louvre Museum
-    { position: { lat: 48.8530, lng: 2.3499 } }, // Cathédrale Notre-Dame de Paris
-  ];
+  openInfo(marker: MapMarker) { 
+    
+    if(this.infoWindow != undefined)
+      this.infoWindow.open(marker);
+  }
+  
+
+  
+  markers: google.maps.MarkerOptions[] = [
+    { position: { lat: -12.091756905999354, lng: -76.95300742653058 },
+      title:'title', 
+      icon:{url:this.tlIcon, scaledSize: new google.maps.Size(50, 50)},
+    }, // Eiffel Tower
+    {
+      position: { lat: -12.091789680114049, lng: -76.95317313713889},
+      title: 'title',
+      icon: { url: this.tlIcon, scaledSize: new google.maps.Size(50, 50)},
+      visible: false
+    }, // Louvre Museum
+    ];
+    private addTrafficLayer(): void {
+      const trafficLayer = new google.maps.TrafficLayer();
+      trafficLayer.setMap(this.mapComponent.googleMap);
+    }
 }
